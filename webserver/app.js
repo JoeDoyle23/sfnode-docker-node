b980fff14c0231a008998fafadb93fc4a6f8ae8d
Joe Doyle <joe@app-press.com>
2015-08-06 15:58:57 -0700

    Minor updates

--- webserver/app.js
+++ webserver/app.js
@@ -1,12 +1,21 @@
-var express = require('express');
+var fs = require("fs");
+var logger = require("morgan");
+var express = require("express");
+
 var app = express();
 
+app.use(logger("dev"));
+
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('Hello from Docker');
+app.get("/", function(req, res) {
+	fs.createReadStream("index.html").pipe(res);
 });
 
-app.set("port", process.env.PORT || 3000);
+app.get("/bender.jpg", function(req, res) {
+	fs.createReadStream("bender.jpg").pipe(res);
+});
+
+app.set("port", process.env.PORT || 5000);
 
 app.listen(app.get("port"), function () {
 	console.log("Super Awesome Server listening on port " + app.get("port"));
